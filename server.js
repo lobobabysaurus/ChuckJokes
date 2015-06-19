6d9ee3508d3fd7dd8c6fd911eb579c3e529daf3a
prs <Phil.R.Simmons@gmail.com>
2015-06-19 17:03:44 -0400

    Added first quote as well as a killer for the socket once it is hit

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var net = require('net');
 net.createServer(function (socket) {
-    socket.write('QOTD');
+    socket.write('I\'m a feminine Eminem.  A slim shady lady.  --Bo Burnham\n');
+    socket.destroy();
 }).listen(17, '127.0.0.1');
+console.log('QOTD Server Running\n');
