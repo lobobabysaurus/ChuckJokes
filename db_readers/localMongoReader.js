a7fcdee7e7f71024e84bed60e5df9b8585d018e1
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-11-04 23:50:49 -0500

    Updated formatting, began database loader

--- db_readers/localMongoReader.js
+++ db_readers/localMongoReader.js
@@ -1,31 +1,28 @@
 var assert = require('assert');
 var Promise = require('promise');
 
-exports.loadDBQuotes = function(){
-    /**
-     * Returns a promise which contains an array of all quotes located within the database
-     */
-    return new Promise(readQuotes);
+exports.loadDBQuotes = function() {
+  return new Promise(readQuotes);
 };
 
-function readQuotes(resolve, reject){
-    /**
-     * Iterates through the mongo quotes table and adds all of them to an array
-     * Resolves the promise with the array when finished
-     */
-    var mongoClient = require('mongodb').MongoClient;
-    var mongoUrl = "mongodb://localhost:27017/NodeQOTDDB";
-    mongoClient.connect(mongoUrl, function (err, db) {
-        assert.equal(err, null, "Error Connecting to the Database");
-        var quotesCollection = db.collection('quotes');
-        quotesCollection.find().toArray(function (err, quotes) {
-            assert.equal(err, null, "Error reading Quotes");
-            var quoteArray = [];
-            quotes.forEach(function (quote) {
-                quoteArray.push(quote["quote"] + " -- " + quote["author"]);
-            });
-            db.close();
-            resolve(quoteArray);
-        });
+/**
+ * Iterates through the mongo quotes table and adds all of them to an array
+ * Resolves the promise with the array when finished
+ */
+function readQuotes(resolve, reject) {
+  var mongoClient = require('mongodb').MongoClient;
+  var mongoUrl = "mongodb://localhost:27017/NodeQOTDDB";
+  mongoClient.connect(mongoUrl, function (err, db) {
+    assert.equal(err, null, "Error Connecting to the Database");
+    var quotesCollection = db.collection('quotes');
+    quotesCollection.find().toArray(function (err, quotes) {
+      assert.equal(err, null, "Error reading Quotes");
+      var quoteArray = [];
+      quotes.forEach(function (quote) {
+          quoteArray.push(quote["quote"] + " -- " + quote["author"]);
+      });
+      db.close();
+      resolve(quoteArray);
     });
+  });
 }
--- server.js
+++ server.js
@@ -7,30 +7,29 @@ var statements = [];
 log("Starting Chuck Norris joke loading");
 var cn = require('./api_readers/chuckNorrisReader.js');
 var jokes = cn.loadJokes().then(function (jokes) {
-    return Promise.resolve(jokes);
+  return Promise.resolve(jokes);
 });
 statements.push(jokes);
 
 log("Starting database quote loading");
 var lq = require('./db_readers/localMongoReader.js');
 var quotes = lq.loadDBQuotes().then(function (quotes) {
-    return Promise.resolve(quotes);
+  return Promise.resolve(quotes);
 });
 statements.push(quotes);
 
 log("Waiting on statement loading to finish");
 var loader = Promise.all(statements);
 loader.then(function (statements){
-        log("Statement Loading Complete");
-        var data = [];
-        for(var i=0;i<statements.length;i++){
-            data = data.concat(statements[i]);
-        }
-        log("Creating QOTD Server");
-        net.createServer(function (socket) {
-            socket.write(data[Math.floor(Math.random()*data.length)] + "\n");
-            socket.destroy();
-        }).listen(54321, '127.0.0.1');
-        log('QOTD Server Running\n');
-    }
-);
+  log("Statement Loading Complete");
+  var data = [];
+  statements.forEach(function (statment) {
+      data = data.concat(statements);
+  })
+  log("Creating QOTD Server");
+  net.createServer(function (socket) {
+      socket.write(data[Math.floor(Math.random()*data.length)] + "\n");
+      socket.destroy();
+  }).listen(54321, '127.0.0.1');
+  log('QOTD Server Running\n');
+});
