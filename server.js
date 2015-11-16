86acaf8694e6aadf17eeb86cda520412405ebef9
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-11-15 23:15:51 -0500

    Updated Server to use use new Chuck Norris database API

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var net = require('net');
 var log = console.log;
 
-var ChuckNorris = require('../chuck-norris-api');
+var ChuckNorris = require('chuck-norris-api');
 
 log('Loading Jokes');
 var chuckJoke = new ChuckNorris();
 
-log('Chuck Joke Server Running\n');
 net.createServer(function (socket) {
     socket.destroy();
-    log('Chuck Joke Server Running\n');
-}).listen(54321, '127.0.0.1');
+}).listen(54321, function() {
+  log('Chuck Joke Server Running');
+});
