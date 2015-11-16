675ae633113f18d451e54cf74b874b916f660068
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-11-16 00:26:57 -0500

    Added Yargs support and created server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var net = require('net');
 var log = console.log;
 
+var argv = require('yargs').argv;
 var ChuckNorris = require('chuck-norris-api');
 
-log('Loading Jokes');
-var chuckJoke = new ChuckNorris();
-
 net.createServer(function (socket) {
-  socket.destroy();
+  var chuckJoke = new ChuckNorris();
+  chuckJoke.getRandom(argv).then(function (data) {
+    socket.write(data.value.joke);
+    socket.destroy();
+  });
 }).listen(54321, function() {
   log('Chuck Joke Server Running');
 });
