d62654696045a4bea64c008052ba392a831171b2
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-11-16 00:41:18 -0500

    added port argument

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ var ChuckNorris = require('chuck-norris-api');
 
 var chuckJoke = new ChuckNorris();
 
+var port = argv.port || 17
 net.createServer(function (socket) {
   chuckJoke.getRandom(argv).then(function (data) {
     socket.write(data.value.joke);
     socket.destroy();
   });
-}).listen(54321, function() {
+}).listen(port, function() {
   log('Chuck Joke Server Running');
 });
