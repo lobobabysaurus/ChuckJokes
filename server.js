cb1d28cea340deaa798b6cec83e04000d5222920
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-11-16 00:34:20 -0500

    Updated version and fixed problem

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var log = console.log;
 var argv = require('yargs').argv;
 var ChuckNorris = require('chuck-norris-api');
 
+var chuckJoke = new ChuckNorris();
+
 net.createServer(function (socket) {
-  var chuckJoke = new ChuckNorris();
   chuckJoke.getRandom(argv).then(function (data) {
     socket.write(data.value.joke);
     socket.destroy();
