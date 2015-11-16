d7276c16b130eaf32b26f9730eaaba5f880db583
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-11-15 22:30:41 -0500

    Changed Joke server to read from the Chuck Norris API with the node api wrapper

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-var cn = require('./api_readers/chuckNorrisReader.js');
 var net = require('net');
 var log = console.log;
-var Promise = require('promise');
 
-var statements = [];
+var ChuckNorris = require('../chuck-norris-api');
 
-log("Starting Chuck Norris joke loading");
-cn.loadJokes().then(function (jokes) {
-  log("Joke Loading Complete");
-  log("Creating Joke Server");
-  net.createServer(function (socket) {
-      socket.write(jokes[Math.floor(Math.random() * jokes.length)] + "\n");
-      socket.destroy();
-  }).listen(54321, '127.0.0.1');
-  log('Chuck Joke Server Running\n');
-});
+log('Loading Jokes');
+var chuckJoke = new ChuckNorris();
+
+log('Chuck Joke Server Running\n');
+net.createServer(function (socket) {
+    socket.destroy();
+    log('Chuck Joke Server Running\n');
+}).listen(54321, '127.0.0.1');
