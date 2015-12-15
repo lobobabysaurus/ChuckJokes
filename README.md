c6fb59fd28d1d326bbfb7d74060d0d007282ab46
Phil Simmons <Phil.R.Simmons@gmail.com>
2015-12-14 22:20:20 -0500

    Made Server RFC-865 compliant by supporting UDP and TCP

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # chuck-jokes
 A Chuck Norris Joke server implemented in Node  
+It supports both IPv4 and IPv6 as well as both TCP and UDP in line with [IETF RFC-865][rfc-link]
 
-## Install  
-The server can be installed globally with `npm install -g chuck-jokes`
+## Install
+The server can be installed globally with `npm install -g chuck-jokes`  
+If a port is not specified when running the server, it will run on port 17 and must be  
+run with root/admin rights.
 
 ## Usage
 chuck-jokes -f [string] -l [string] -o [string] -e [string] -p [number]  
@@ -14,4 +17,6 @@ Options:
   -e, --exclude  Comma separated list of categories to exclude jokes    [string]  
   -p, --port     Port server will run on                           [default: 17]  
   -h, --help     Show help                                             [boolean]  
-  --version      Show version number                                    [number]
+  --version      Show version number                                   [boolean]
+
+[rfc-link]: https://tools.ietf.org/html/rfc865
--- server.js
+++ server.js
@@ -1,38 +1,36 @@
 #! /usr/bin/env node
-
 var net = require('net');
-var log = console.log;
+var udp = require('dgram');
 
 var argv = require('yargs')
             .usage('Usage: $0 -f [string] -l [string] -o [string] -e [string] -p [num]')
-            .options(
-              {
-                'f': {
-                  alias: 'first',
-                  describe: 'First name to replace Chuck\'s in jokes',
-                  string: true
-                },
-                'l': {
-                  alias: 'last',
-                  describe: 'Last name to replace Chuck\'s in jokes',
-                  string: true
-                },
-                'o': {
-                  alias: 'only',
-                  describe: 'Comma separated list of categories to limit jokes',
-                  string: true
-                },
-                'e': {
-                  alias: 'exclude',
-                  describe: 'Comma separated list of categories to exclude jokes',
-                  string: true
-                },
-                'p': {
-                  alias: 'port',
-                  describe: 'Port server will run on',
-                  default: 17
-                }
-              })
+            .options({
+              'f': {
+                alias: 'first',
+                describe: 'First name to replace Chuck\'s in jokes',
+                string: true
+              },
+              'l': {
+                alias: 'last',
+                describe: 'Last name to replace Chuck\'s in jokes',
+                string: true
+              },
+              'o': {
+                alias: 'only',
+                describe: 'Comma separated list of categories to limit jokes',
+                string: true
+              },
+              'e': {
+                alias: 'exclude',
+                describe: 'Comma separated list of categories to exclude jokes',
+                string: true
+              },
+              'p': {
+                alias: 'port',
+                describe: 'Port server will run on',
+                default: 17
+              }
+            })
             .help('h')
             .alias('h', 'help')
             .version(function() {
@@ -49,19 +47,48 @@ if (argv.f) options.firstName = argv.f;
 if (argv.l) options.lastName = argv.l;
 if (argv.o) options.limitTo = argv.o;
 if (argv.e) options.exclude = argv.e;
+var port = argv.p;
 
-net.createServer(function (socket) {
-  chuckJoke.getRandom(options).then(function (data) {
-    socket.write(data.value.joke + "\n");
-    socket.destroy();
+/**
+ * Message to display on successful connection
+ *
+ * @param type String UDP or TCP
+ * @returns String message saying the protocol and port the server is using
+ */
+function successMessage(type) {
+  console.log("Chuck Jokes Server Running on " + type + " Port " + port);
+}
+
+//Setup a server for tcp on port 17
+net.createServer( function (tcp_socket) {
+    chuckJoke.getRandom(options).then(function (data) {
+      tcp_socket.write(data.value.joke + "\n");
+      tcp_socket.destroy();
+    })
+  })
+  .listen(port, '::', function() { successMessage('TCP')})
+  .on('error', function(error) {
+    if (error.code === 'EACCES' && argv.p < 1024) {
+      console.error('Running server on ports below 1024 must be done as root');
+    }
+    else {
+      console.error('There has been an issue while starting the TCP server');
+    }
   });
-}).listen(argv.p, function() {
-  log('Chuck Joke Server Running on Port ' + argv.p);
-}).on('error', function(err) {
-  if (err.code === 'EACCES' && argv.p < 1024) {
-    log('Running server on ports below 1024 must be done as root');
-  }
-  else {
-    log('There has been an error starting the server');
-  }
-});
+
+// Setup a server for udp on port 17
+var udp_socket = udp.createSocket('udp6');
+udp_socket
+  .on('message', function (message, remote) {
+    chuckJoke.getRandom(options).then(function (data) {
+      var joke = new Buffer(data.value.joke+"\n");
+      udp_socket.send(joke, 0, joke.length, remote.port, remote.address);
+    });
+  })
+  .on('listening', function() { successMessage('UDP') })
+  .on('error', function(error) {
+    if (!error.code === 'EACCES' && argv.p < 1024) {
+      console.error('There has been an issue while starting the UDP server');
+    }
+  })
+  .bind(port);
